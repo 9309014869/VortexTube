99ab4ed352c185c519e4ee85e42ce5b32ed0c205
TEJAS GAJANAN KAWARKHE <112716745+9309014869@users.noreply.github.com>
2025-04-03 15:24:39 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,//http://localhost:5173
+        origin: https://vortextube-frontend.onrender.com,//http://localhost:5173
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     })
